962647ce2d9e8ba3cfb59c9189afe5aadfe95a0d
Alejandro Ruiz <alejandroruizprieto@alumnos.tuniversformacion.es>
2023-11-06 09:30:49 +0100

    hicimos scripts y ahora con estilos

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -16,3 +16,14 @@ const abrirNuevaVentana = () => {
 const abrirMismaVentana = () => {
     window.open("https://www.tuniversformacion.es", "_self")
 };
+
+//Acceder a las propiedades de los objetos
+document.getElementById("anchoVentana").textContent = window.innerWidth;
+
+document.getElementById("urlActual").textContent = window.location.href;
+
+//Devuelve la informacion del navegador
+document.getElementById("navegadorInfo").textContent = window.navigator.userAgent;
+
+//Numero de enlaces
+document.getElementById("numeroEnlaces").textContent = document.links.length;
